6c5eed0c2a05b0d925962a67537686e4a0e630bc
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-07 17:05:09 +0200

    Aggiunto un indice numerico ai miei commenti ad inizio pagina in modo che possano essere ricollegati al mio codice più facilmente.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-// Creo un array vuoto che deve contenere i miei 5 numeri random.
-// creo un ciclo while per forza perchè altrimenti non so come ovviare alla possibilità
-// che vengano creati 2 numeri uguali.
+// 1) Creo un array vuoto che deve contenere i miei 5 numeri random.
+// 1) creo un ciclo while per forza perchè altrimenti non so come ovviare alla possibilità
+// 1) che vengano creati 2 numeri uguali.
 // =========================================================================================
-// collego gli elementi che penso possano servirmi dal file HTML a JS creando delle variabili
-// suppongo siano tutti gli elementi con un id btw.
+// 2) collego gli elementi che penso possano servirmi dal file HTML a JS creando delle variabili
+// 2) suppongo siano tutti gli elementi con un id btw.
 // =========================================================================================
-// creo una variabile stringaHtml vuota e con un ciclo for gli passo gli elementi dell'array
-// di cinque numeri random, trasformandoli in una stringa HTML, scrivo nel mio file HTML 
-// dentro a numberList usando inner.HTML 
+// 3) creo una variabile stringaHtml vuota e con un ciclo for gli passo gli elementi dell'array
+// 3) di cinque numeri random, trasformandoli in una stringa HTML, scrivo nel mio file HTML 
+// 3) dentro a numberList usando inner.HTML 
 // =========================================================================================
 // COUNTDOWN di 30 secondi
-// Creo una variabile per i secondi che deve essere === 30.
-// Creo un event timer con setInterval in modo che ogni secondo scenda di 1
-// lo mostro in HTML usando l'id countdown che ho "preso" in precedenza.
+// 4) Creo una variabile per i secondi che deve essere === 30.
+// 4) Creo un event timer con setInterval in modo che ogni secondo scenda di 1
+// 4) lo mostro in HTML usando l'id countdown che ho "preso" in precedenza.
 
 
 
 
-// ARRAY VUOTO + CICLO WHILE PER CREARE ARRAY DI 5 NUMERI RANDOM
+// 1) ARRAY VUOTO + CICLO WHILE PER CREARE ARRAY DI 5 NUMERI RANDOM
 let arrayDiCinqueNumeriVuoto = [];
 
 while (arrayDiCinqueNumeriVuoto.length < 5) {
@@ -28,22 +28,24 @@ while (arrayDiCinqueNumeriVuoto.length < 5) {
 
 }
 
-// ELEMENTI HTML CHE COLLEGO A JS 
+// 2) ELEMENTI HTML CHE COLLEGO A JS 
 const countdown = document.querySelector("#countdown");
 const numbersList = document.querySelector("#numbers-list");
 const answersForm = document.querySelector("#answers-form");
 const message = document.querySelector("#message");
 
 
-// STRINGA VUOTA DENTRO LA QUALE CICLO GLI ELEMENTI DELL'ARRAY
-// TRASFORMANDOLI IN <li>
+// 3) STRINGA VUOTA DENTRO LA QUALE CICLO GLI ELEMENTI DELL'ARRAY
+// 3) TRASFORMANDOLI IN <li>
 let stringaHtml = "";
 for (let i = 0; i <= 4; i++) {
     stringaHtml = stringaHtml + `<li>${arrayDiCinqueNumeriVuoto[i]}</li>`
 }
 
-// CON INNERHTML SCRIVO I NUMERI ALL'INTERNO DI number-list IN HTML
+// 3) CON INNERHTML SCRIVO I NUMERI ALL'INTERNO DI number-list IN HTML
 numbersList.innerHTML = stringaHtml;
     
-// VARIABILE PER I SECONDI 
+// 4) VARIABILE PER I SECONDI 
 let secondi = 30;
+
+// 4) VARIABILE PER IL TIMER 
