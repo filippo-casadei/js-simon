4f8484c5d92da0395e8837fa7ec3a4a85291e310
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-07 16:05:01 +0200

    Modificato il file script.js : - create le variabili  nel file script.js degli elementi con id univoco in HTML che presumo serviranno.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // Creo un array vuoto che deve contenere i miei 5 numeri random.
 // creo un ciclo while per forza perchè altrimenti non so come ovviare alla possibilità
 // che vengano creati 2 numeri uguali.
+// =========================================================================================
+// collego gli elementi che penso possano servirmi dal file HTML a JS creando delle variabili
+// suppongo siano tutti gli elementi con un id btw.
+// =========================================================================================
+// 
 
+// ARRAY VUOTO + CICLO WHILE PER CREARE ARRAY DI 5 NUMERI RANDOM
 let arrayDiCinqueNumeriVuoto = [];
 
 while (arrayDiCinqueNumeriVuoto.length < 5) {
@@ -11,3 +17,9 @@ while (arrayDiCinqueNumeriVuoto.length < 5) {
     }
 
 }
+
+// ELEMENTI HTML CHE COLLEGO A JS 
+const countdown = document.querySelector("#countdown");
+const numbersList = document.querySelector("#numbers-list");
+const answersForm = document.querySelector("#answers-form");
+const message = document.querySelector("#message");
